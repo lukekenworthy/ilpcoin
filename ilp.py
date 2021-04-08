ef170b6d151f4102dcf60445bbde742e3a0b9f39
Jordan Barkin <jordanbarkin@me.com>
2021-04-08 02:15:41 -0400

    added basic checking functionality

--- ilp.py
+++ ilp.py
@@ -9,7 +9,7 @@ MAX_TIME = 300
 class IlpSolution:
     def __init__(self, solved_ilp):
         self.ilp_id = solved_ilp.uid
-        self.results = [(v.name, v.x) for v in solved_ilp.mip_ilp.vars]
+        self.variable_results = [(v.name, v.x) for v in solved_ilp.mip_ilp.vars]
 
     @classmethod
     def deserialize(cls, raw_bytes : bytes):
@@ -27,12 +27,13 @@ class Ilp:
     # Contains the subset of the state from the more complex Ilp class that we want to serialize and send around
     class SerializableIlp:
         # create these from full-blown Ilp objects
-        def __init__(self, full_ilp):
+        def __init__(self, full_ilp, k):
             # we need temp files to use mip's built in serialization; this is an ugly hack
             with  tempfile.NamedTemporaryFile(suffix=".lp") as tempOutputFile:
                 full_ilp.mip_ilp.write(tempOutputFile.name)
                 self.serialized_ilp = tempOutputFile.read()
                 self.uid = full_ilp.uid
+                self.k = k
 
         # convert back to a full ilp.
         # any solution progress will be lost.
@@ -41,12 +42,13 @@ class Ilp:
                 tempOutputFile.write(self.serialized_ilp)
                 deserialized_ilp = mip.Model()
                 deserialized_ilp.read(tempOutputFile.name)
-                return Ilp(self.uid, deserialized_ilp)
+                return Ilp(self.uid, deserialized_ilp, self.k)
 
     # create an Ilp from a mip model
-    def __init__(self, uid : int, mip_ilp : mip.Model):
+    def __init__(self, uid : int, mip_ilp : mip.Model, k : float):
         self.mip_ilp = mip_ilp
         self.uid = uid
+        self.k = k
 
     def setId(self, uid: int):
         self.uid = uid
@@ -71,8 +73,19 @@ class Ilp:
     def deserialize(cls, raw_bytes : bytes):
         return pickle.loads(raw_bytes).to_full_ilp()
 
+    def __eval_objective_function(solution):
+        objective_function = self.mip_ilp.objective
+        res = objective_function.const
+        for v, coeff in self.mip_ilp.expr.items():
+            v_value = solution.variable_results[v.name]
+            res += v_value * coeff
+
+        return res
+
     def check(self, solution : IlpSolution):
-        # how the heck do we do this?
-        pass
+        solution_value = __eval_objective_function(solution)
+        return solution_value < k
+
+
 
 
