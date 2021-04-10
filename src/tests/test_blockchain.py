9dce0bd8c743b9e25847485a4b4456cf2c822b37
Lavanya Singh <lsingh@college.harvard.edu>
2021-04-09 23:20:27 -0400

    added blockchain library correctness tests

--- src/tests/test_blockchain.py
+++ src/tests/test_blockchain.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 import pytest
 from ilpcoin.common.blockchain import Transaction, Block, Blockchain
 
@@ -47,4 +48,49 @@ class SerializationTests(unittest.TestCase):
         chain1 = Blockchain([block], 5)
         chain2 = Blockchain().deserialize(chain1.serialize())
         self.assertEqual(chain1, chain2)
+
+class BlockchainTests(unittest.TestCase):
+
+    def setup(self):
+        self.previous = Block(transactions=[], prev_hash='', nonce=0)
+        self.b = Block(transactions=[], prev_hash = self.previous.hash(), nonce=41)
+        self.hardness = 1
+
+    def test_validate_nonce(self):
+        self.setup()
+        self.assertTrue(self.b.validate_nonce(self.hardness))
+
+    def test_falsify_nonce(self):
+        self.setup()
+        self.b.nonce = 0
+        self.assertFalse(self.b.validate_nonce(self.hardness))
+
+    def test_validate_POW(self):
+        self.setup()
+        self.assertTrue(self.b.validate_POW(self.previous, self.hardness))
+
+    def test_falsify_POW(self):
+        self.setup()
+        self.b.prev_hash = ''
+        self.assertFalse(self.b.validate_POW(self.previous, self.hardness))
+
+    def test_verify_transaction(self):
+
+        # lav mined this block
+        t0 = Transaction().initialize(sender="lav", receiver="lav", amount=5)
+
+        # some transactions
+        t1 = Transaction().initialize(sender="lav", receiver="luke", amount=3)
+        t2 = Transaction().initialize(sender="luke", receiver="lav", amount=1)
+        t3 = Transaction().initialize(sender="lav", receiver="luke", amount=1)
+        t4 = Transaction().initialize(sender="lav", receiver="luke", amount=10)
+
+        b = Blockchain(blocks=[Block(transactions=[t0, t1, t2, t3, t4])])
+
+        # ignoring t4
+        self.assertTrue(b.verify_transaction(t3, 2, 3))
+
+        # including t4
+        self.assertFalse(b.verify_transaction(t3, 2, 5))
+
 
