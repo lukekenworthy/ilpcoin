f73a7666821de8e1c3c36cc393d292cc67b6cbd9
Lavanya Singh <lsingh@college.harvard.edu>
2021-04-10 20:26:27 -0400

    added logging support

--- src/ilpcoin/verifier/__main__.py
+++ src/ilpcoin/verifier/__main__.py
@@ -5,7 +5,7 @@ import logging
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-id", help="Node Id", type=int, default=0)
-    parser.add_argument("-t", help=("Optional testing flag"), default=False)
+    parser.add_argument("-t", help=("Optional testing flag"), default=True)
     parser.add_argument("-host", help="Server hostname", type=str, default='localhost')
     parser.add_argument("-port", help="Server port number", type=int, default='8000')
     args = parser.parse_args()
@@ -13,10 +13,10 @@ def main():
     HOST = args.host
     PORT = int(args.port)
 
-    logging.basicConfig(filename='verifier.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename='logs/verifier.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
     logging.basicConfig(level=logging.DEBUG)
 
-    verifier = Verifier(id, host=HOST, port=PORT, testing=args.t)
+    verifier = Verifier(id=args.id, host=HOST, port=PORT, testing=args.t)
     verifier.run()
 
 if __name__ == '__main__':
