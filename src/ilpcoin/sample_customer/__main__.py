8fdcadf903e953960d1d75d44711224e48f1b710
Jordan Barkin <jordanbarkin@me.com>
2021-04-17 08:49:29 -0400

    very simple customer script

--- src/ilpcoin/sample_customer/__main__.py
+++ src/ilpcoin/sample_customer/__main__.py
@@ -1,9 +1,12 @@
 import argparse
 import requests
 from ilpcoin.common.sample_ilps.traveling_salesman import traveling_salesman
+from ilpcoin.common.constants import *
 
 def main():
-    pass
+    demo_ilp = traveling_salesman()
+    r = requests.post(QUEUE_HOST + ":" + str(QUEUE_PORT) + "/add_ilp",  data = {'ilp': demo_ilp.serialize()})
+    print("Received: " + str(r))
 
 if __name__ == '__main__':
     main()
