1f0a0edc31bd4c6d18595a99a07b452e97c961df
Jordan Barkin <jordanbarkin@me.com>
2021-04-17 08:02:35 -0400

    new functionality

--- src/ilpcoin/ilp_queue/ilp_queue.py
+++ src/ilpcoin/ilp_queue/ilp_queue.py
@@ -1,27 +1,31 @@
-from flask import Flask
+from typing import List, Set
+from flask import Flask, request
+import requests
 from ilpcoin.common.ilp import *
 import queue
+import logging
 import threading
+from random import sample
 
 # how many verifiers required to pop the top of the queue
 VERIFIERS_REQUIRED = 3
 
 # ToDo: All the database stuff
-# ToDo: The "DNS" service that can find neighbors
 # ToDo: Authentication
 # ToDo: Input sanitization
-# ToDo
 
 # IlpQueue represents a queue of ilps. 
 # This state needs to be written to databases. 
 class IlpQueue:
-    def __init__(self):
+    def __init__(self, initial_verifiers : List[str] = []):
         self.q = queue.Queue() # the queue 
         self.top : Optional[Ilp] = None  # the top ilp, currently worked on by miners
         self.count : int = 0   # the number of verifiers who have provided solutions for self.top
         self.ilp_history : dict[int, Ilp] = {} # a dictionary mapping all previous ilp IDs to their ilp problems. 
-        self.soln_history : dict[int, IlpSolution] = {} #  a dictionary mapping all previous solved ilp IDs to their solutions. Right now, this is never updated
+        # self.soln_history : dict[int, IlpSolution] = {} #  a dictionary mapping all previous solved ilp IDs to their solutions. Right now, this is never updated
         self.last_used_uid = 0 # last uid; let's not repeat uids!   
+        self.verifiers = initial_verifiers
+        self.last_used_verifier = 0
 
     # Eventually, this will query a database and reconstruct the Ilp from there
     @classmethod
@@ -36,12 +40,20 @@ class IlpQueue:
         self.q.put(ilp)
         self.ilp_history[ilp.get_id()] = ilp
         return ilp.get_id()
+    
+    # Return a verifier ip, different from the last time
+    def get_verifier_ip(self): 
+        self.last_used_verifier += 1
+        return self.verifiers[self.last_used_verifier % len(self.verifiers)]
+
+    def add_verifer(self, ip): 
+        self.verifiers.append(ip)
 
     # Return the current ilp to work on
     def get_top(self) -> Optional[Ilp]:
         return self.top
 
-    # ToDo
+    # Get historical ilp
     def lookup_ilp(self, id : int) -> Optional[Ilp]:
         # do we want to query a verifier here?
         try:
@@ -49,16 +61,16 @@ class IlpQueue:
         except:
             return None
 
-    def lookup_solution(self, id : int)  -> Optional[IlpSolution]:
-        # do we want to query a verifier here?
-        # how do we get these, anyway?
-        try:
-            return self.soln_history[id]
-        except:
-            return None
+    # def lookup_solution(self, id : int)  -> Optional[IlpSolution]:
+    #     # do we want to query a verifier here?
+    #     # how do we get these, anyway?
+    #     try:
+    #         return self.soln_history[id]
+    #     except:
+    #         return None
 
     def __complete_item(self) -> None:
-        if not q.empty():
+        if not self.q.empty():
             self.top = self.q.get()
         else:
             self.top = None
@@ -87,9 +99,11 @@ app = Flask(__name__)
 @app.route('/add_ilp', methods=['POST'])
 def add_ilp():
     new_ilp_serialized = request.form.get('ilp')
+    if not new_ilp_serialized:
+        return ERROR_FAILURE
     new_ilp = Ilp.deserialize(new_ilp_serialized)
     ilp_queue.add(new_ilp)
-    return "Confirmed"
+    return str(new_ilp.get_id())
 
 @app.route('/get_top_ilp', methods=['GET'])
 def get_top_ilp():
@@ -104,34 +118,26 @@ def get_ilp_by_id(uid):
 # will return
 @app.route('/get_solution_by_id/<uid>', methods=['GET'])
 def get_solution_by_id(uid):
-    result = ilp_queue.lookup_solution(uid)
-    return result.serialize() if result else "Unsolved"
+    r = requests.get(ilp_queue.get_verifier_ip() + "/get_neighbors/" + str(uid))
+    return r.content
 
 @app.route('/verify_ilp/<uid>', methods=['GET'])
 def verify_ilp(uid):
-    return "true" if ilp_queue.incr_count(uid) else "False"
+    return "True" if ilp_queue.incr_count(uid) else "False"
 
-@app.route('/register_verifier/<uid>', methods=['GET'])
-def register_verifier(uid): 
-    pass
-    # oh hallo I am a new verifier todo 
+# add a verifier to the list
+@app.route('/register_verifier/<address>', methods=['GET'])
+def register_verifier(address): 
+    if not address in ilp_queue.verifiers: 
+        ilp_queue.add_verifer(address)
+    return str(True)
 
+# Return n verifier IPs, or all the verifiers if < n
 @app.route('/get_neighbors/<n>', methods=['GET'])
 def get_neighbors(n):
-    pass
-    # todo
-    # queue can yeet off verifiers that it hasn't heard from 
+    try: 
+        res = sample(ilp_queue.verifiers, n)
+    except: 
+        res = ilp_queue.verifiers
 
-# @param VERIFIERS_REQUIRED
-
-# For customers:
-# /add_ilp(the_ilp)
-# /get_solved_ilp(id) → Solution; raises error if not yet solved
-
-# For miners:
-# 	/get_top_ilp() → ILP
-
-# For verifiers:
-# 	/get_ilp_by_id(id) → ILP
-# 	/verification(id) →  bool for whether its top; queue increments its verified count
-# 	/give_neighbors(n) → list of random subset of n verifiers
+    return pickle.dumps(res)
