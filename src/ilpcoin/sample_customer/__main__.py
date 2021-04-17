48fa2a9d0625670206dcc6d8b725f98afe7e2f65
Jordan Barkin <jordanbarkin@me.com>
2021-04-17 09:37:35 -0400

    more sample customer

--- src/ilpcoin/sample_customer/__main__.py
+++ src/ilpcoin/sample_customer/__main__.py
@@ -7,32 +7,33 @@ from ilpcoin.common.constants import *
 from ilpcoin.common.ilp import *
 
 def main():
+    queue_url = "http://" + QUEUE_HOST + ":" + str(QUEUE_PORT)
     print("Sending TSM")
     demo_ilp = traveling_salesman()
-    r = requests.post(QUEUE_HOST + ":" + str(QUEUE_PORT) + "/add_ilp",  data = {'ilp': demo_ilp.serialize()})
-    tsm_id = r.content
-    print("Received id of: " + str(tsm_id))
+    r = requests.post(queue_url+ "/add_ilp",  data = {'ilp': demo_ilp.serialize_s()})
+    tsm_id = int(r.content)
+    print("Received id of: " + str((tsm_id)))
 
     print("Sending Knapsack")
     demo_ilp = knapsack()
-    r = requests.post(QUEUE_HOST + ":" + str(QUEUE_PORT) + "/add_ilp",  data = {'ilp': demo_ilp.serialize()})
-    knapsack_id = r.content
+    r = requests.post(queue_url+ "/add_ilp",  data = {'ilp': demo_ilp.serialize_s()})
+    knapsack_id = int(r.content)
     print("Received id of: " + str(knapsack_id))
 
     print("Looking up ilps: ")
-    tsm_raw = requests.get(QUEUE_HOST + ":" + str(QUEUE_PORT) + "/get_ilp_by_id/" + str(tsm_id)).content
-    tsm_back = Ilp.deserialize(tsm_raw)
-    knapsack_raw = requests.get(QUEUE_HOST + ":" + str(QUEUE_PORT) + "/get_ilp_by_id/" + str(knapsack_id)).content
-    knapsack_back = Ilp.deserialize(knapsack_raw)
+    tsm_raw = requests.get(queue_url+ "/get_ilp_by_id/" + str(tsm_id)).text
+    tsm_back = Ilp.deserialize_s(tsm_raw)
+    knapsack_raw = requests.get(queue_url+ "/get_ilp_by_id/" + str(knapsack_id)).text
+    knapsack_back = Ilp.deserialize_s(knapsack_raw)
 
     print("Sleeping for 30 seconds to allow chain to do its thing.")
     time.sleep(30)
 
     print("Looking up solutions: ")
-    r = requests.get(QUEUE_HOST + ":" + str(QUEUE_PORT) + "/get_solution_by_id/" + str(tsm_id))
-    tsm_soln = IlpSolution.deserialize(r.content)
-    r = requests.get(QUEUE_HOST + ":" + str(QUEUE_PORT) + "/get_solution_by_id/" + str(knapsack_id))
-    knapsack_soln = IlpSolution.deserialize(r.content)
+    r = requests.get(queue_url+ "/get_solution_by_id/" + str(tsm_id))
+    tsm_soln = IlpSolution.deserialize_s(r.text)
+    r = requests.get(queue_url+ "/get_solution_by_id/" + str(knapsack_id))
+    knapsack_soln = IlpSolution.deserialize_s(r.text)
 
     print("Got solutions")
     print("Tsm correct?: " + str(tsm_back.check(tsm_soln)))
