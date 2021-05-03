74e22d81d2ca63d7fc67b2c19204d8ad1f1f6d0f
Jordan Barkin <jordanbarkin@me.com>
2021-05-02 22:57:46 -0400

    random knapsack generator

---
+++ src/ilpcoin/common/sample_ilps/random_knapsack.py
@@ -0,0 +1,25 @@
+# example inspired by https://docs.python-mip.com/en/latest/examples.html
+from mip import Model, xsum, maximize, BINARY, minimize
+from ilpcoin.common.ilp import Ilp
+import random
+
+def random_knapsack(): 
+
+    # constants 
+    p = [random.randint(0,35) for x in range(6)]
+    w = [random.randint(0,35) for x in range(6)]
+    c, I = 47, range(len(w))
+
+    m = Model("knapsack")
+
+    # add variables
+    x = [m.add_var(var_type=BINARY) for i in I]
+
+    # set objective function
+    m.objective = maximize(xsum(p[i] * x[i] for i in I))
+
+    # add constraints
+    m += xsum(w[i] * x[i] for i in I) <= c
+
+    
+    return Ilp(m, 40, -1, True)
